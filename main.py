b41e6d2c13f2a6860e63f984a5e64b90faab6c50
alejoboga20 <alejoboga19@gmail.com>
2024-01-04 15:01:03 -0300

    use conversation summary

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory
 
 from dotenv import load_dotenv
 load_dotenv()
 
 chat = ChatOpenAI()
 
-memory = ConversationBufferMemory(
+memory = ConversationSummaryMemory(
     memory_key="messages",
     return_messages=True,
-    chat_memory=FileChatMessageHistory("messages.json")
+    llm=chat,
+    # chat_memory=FileChatMessageHistory("messages.json")
 )
 
 prompt = ChatPromptTemplate(
