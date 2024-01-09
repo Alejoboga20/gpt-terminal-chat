6414cd863634e02de0de1a960eda657c52026dff
alejoboga20 <alejoboga19@gmail.com>
2024-01-09 10:28:02 -0300

    load content with langchain

--- facts-embeddings/main.py
+++ facts-embeddings/main.py
@@ -1,2 +1,9 @@
+from langchain.document_loaders import TextLoader
+
 from dotenv import load_dotenv
 load_dotenv()
+
+loader = TextLoader("facts-embeddings/facts.txt")
+docs = loader.load()
+
+print(docs)
